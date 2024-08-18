5c621b0de22cc0945f67b1fb3fbb80ae8728416f
Jack Nash <jacknash42@gmail.com>
2024-08-18 15:10:41 +1000

    feat: impl perpetual connection, even upon disconnection

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 use discord_rich_presence::{activity, DiscordIpc, DiscordIpcClient};
+use std::{thread, time::Duration};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = DiscordIpcClient::new("843514276383031296")?;
-
-    client.connect()?;
-    client.set_activity(activity::Activity::new().state("foo").details("bar"))?;
+fn main() {
+    let mut client = match DiscordIpcClient::new("843514276383031296") {
+        Ok(client) => client,
+        Err(err) => {
+            println!("Failed to create client: {}", err);
+            return;
+        }
+    };
 
     loop {
-        println!("foo");
+        match client.connect() {
+            Ok(()) => {
+                println!("Connected to Discord");
+            }
+            Err(err) => {
+                println!("Failed to connect: {}", err);
+                thread::sleep(Duration::from_secs(5)); // wait 5 seconds before retrying
+                continue;
+            }
+        }
+
+        loop {
+            match client.set_activity(activity::Activity::new().state("foo").details("bar")) {
+                Ok(()) => println!("Set activity"),
+                Err(err) => {
+                    println!("Failed to set activity: {}", err);
+                    break; // if setting activity fails, break out of the loop and try to reconnect
+                }
+            }
+
+            thread::sleep(Duration::from_secs(15)); // update activity every 15 seconds
+        }
     }
 }
