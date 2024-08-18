843b9d47098a41a8be0444d995e0b891b5dc4a76
Jack Nash <jacknash42@gmail.com>
2024-08-18 21:58:30 +1000

    feat: add basic tray application

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,17 @@ use discord_rich_presence::{
     activity::{self, Assets, Button, Timestamps},
     DiscordIpc, DiscordIpcClient,
 };
-use std::{thread, time::Duration};
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread,
+    time::Duration,
+};
+use tray_item::{IconSource, TIError, TrayItem};
 
-fn presence() {
+fn presence(terminate: Arc<AtomicBool>) {
     let mut client = match DiscordIpcClient::new("1143833637767348304") {
         Ok(client) => client,
         Err(err) => {
@@ -17,6 +25,11 @@ fn presence() {
     let timestamp = Timestamps::new().start(chrono::Utc::now().timestamp());
 
     loop {
+        if terminate.load(Ordering::SeqCst) {
+            println!("Presence thread terminating.");
+            break;
+        }
+
         match client.connect() {
             Ok(()) => {
                 println!("Connected to Discord");
@@ -29,6 +42,11 @@ fn presence() {
         }
 
         loop {
+            if terminate.load(Ordering::SeqCst) {
+                println!("Presence thread terminating.");
+                break;
+            }
+
             match client.set_activity(
                 activity::Activity::new()
                     .details("Wanna play all things mafia?")
@@ -57,6 +75,37 @@ fn presence() {
     }
 }
 
+fn tray(terminate: Arc<AtomicBool>) -> Result<(), TIError> {
+    let mut tray = TrayItem::new("Mafia Engine", IconSource::Resource(""))?;
+
+    let inner = tray.inner_mut();
+    inner.add_quit_item("Quit");
+    inner.display();
+
+    let terminate_clone = Arc::clone(&terminate);
+    tray.add_menu_item("Quit", move || {
+        terminate_clone.store(true, Ordering::SeqCst);
+        std::process::exit(0);
+    })?;
+
+    Ok(())
+}
+
 fn main() {
-    presence();
+    let terminate = Arc::new(AtomicBool::new(false));
+
+    let terminate_presence = Arc::clone(&terminate);
+    let _ = thread::spawn(move || {
+        presence(terminate_presence);
+    });
+
+    match tray(terminate) {
+        Ok(_) => {
+            println!("Tray icon created successfully");
+        }
+        Err(err) => {
+            println!("Failed to create tray icon: {}", err);
+            return;
+        }
+    }
 }
